2fde50b946ef185826de2765f67b3b90de4ee1d2
Nail Islamov <nislamov@atlassian.com>
2018-03-14 10:37:30 +1100

    Add status to Broker interface

--- pkg/stateful/task/broker.go
+++ pkg/stateful/task/broker.go
@@ -3,8 +3,19 @@ package task
 import "encoding/json"
 
 type Broker interface {
-	CreateInstance(instanceId string, parameters json.RawMessage) (json.RawMessage, error)
-	UpdateInstance(instanceId string, parameters json.RawMessage) (json.RawMessage, error)
-	DeleteInstance(instanceId string, parameters json.RawMessage) error
-	// TODO add methods for bindings
+	CreateInstance(instanceId string, parameters json.RawMessage) (ExecutionState, json.RawMessage, error)
+	UpdateInstance(instanceId string, parameters json.RawMessage) (ExecutionState, json.RawMessage, error)
+	DeleteInstance(instanceId string, parameters json.RawMessage) (ExecutionState, error)
+	//CreateBinding(instanceId string, instanceParameters json.RawMessage, instanceOutputs json.RawMessage,
+	//	bindingId string, bindindParameters json.RawMessage) (ExecutionState, json.RawMessage, error)
+	//DeleteBinding(instanceId string, instanceParameters json.RawMessage, instanceOutputs json.RawMessage,
+	//	bindingId string, bindindParameters json.RawMessage) (ExecutionState, error)
 }
+
+type ExecutionState string
+
+const (
+	ExecutionStateSuccess    ExecutionState = "Success"
+	ExecutionStateFailed     ExecutionState = "Failed"
+	ExecutionStateInProgress ExecutionState = "InProgress"
+)
