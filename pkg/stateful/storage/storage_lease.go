439cbc38fdb4584e0fef78cd302291fc88adc64a
Yiyang Chen <ychen@atlassian.com>
2018-03-15 13:12:10 +1100

    fix compile errors

--- pkg/stateful/storage/storage_lease.go
+++ pkg/stateful/storage/storage_lease.go
@@ -1,10 +1,8 @@
 package storage
 
-import "github.com/nilebox/broker-server/pkg/stateful/storage"
-
 type StorageWithLease interface {
-	storage.Storage
-	UpdateInstanceState(instanceId string, state storage.InstanceState, err string) error
+	Storage
+	UpdateInstanceState(instanceId string, state InstanceState, err string) error
 	ExtendLease(instanceIds []string) error
-	LeaseAbandonedInstances(maxBatchSize uint32) ([]*storage.InstanceRecord, error)
+	LeaseAbandonedInstances(maxBatchSize uint32) ([]*InstanceRecord, error)
 }
