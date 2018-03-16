74de2fc954f57f527e70c886960c99f17ad0295e
Nail Islamov <nislamov@atlassian.com>
2018-03-16 12:57:15 +1100

    Remove types

--- pkg/stateful/task/broker.go
+++ pkg/stateful/task/broker.go
@@ -6,16 +6,10 @@ type Broker interface {
 	CreateInstance(instanceId string, parameters json.RawMessage) (json.RawMessage, error)
 	UpdateInstance(instanceId string, parameters json.RawMessage) (json.RawMessage, error)
 	DeleteInstance(instanceId string, parameters json.RawMessage) error
+
+	// TODO bindings
 	//CreateBinding(instanceId string, instanceParameters json.RawMessage, instanceOutputs json.RawMessage,
 	//	bindingId string, bindindParameters json.RawMessage) (ExecutionState, json.RawMessage, error)
 	//DeleteBinding(instanceId string, instanceParameters json.RawMessage, instanceOutputs json.RawMessage,
 	//	bindingId string, bindindParameters json.RawMessage) (ExecutionState, error)
 }
-
-type ExecutionState string
-
-const (
-	ExecutionStateSuccess    ExecutionState = "Success"
-	ExecutionStateFailed     ExecutionState = "Failed"
-	ExecutionStateInProgress ExecutionState = "InProgress"
-)
